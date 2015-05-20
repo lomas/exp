46d086c9a803afd30569e9e0fb2a426dcac4119c
lms <lomas@live.cn>
2015-05-20 17:16:30 +0800

    add bat file for rulemine

---
+++ rulemine.bat
@@ -0,0 +1,6 @@
+::cluster number K is a global parameter across all modules
+python siftfeat.py
+python clustersift.py
+python create_transaction.py
+python tapriori.py
+python detctwithrule.py src.jpg out.jpg
